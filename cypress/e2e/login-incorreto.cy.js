c05a45c79496b09f0ebe5745327c8b7538d64b9c
ArrozbR <pedroblamires@gmail.com>
2025-08-28 12:45:20 -0300

    Add stub

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,10 +1,19 @@
 describe('Página de Login', () => {
   beforeEach(() =>{
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.visit('https://adopet-frontend-cypress.vercel.app');
     cy.get('[data-test="login-button"]').click();
+    cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+      statusCode: 400, }).as('stubPost');
   })
 
   it('Deve preencher os campos de login incorretamente e exibir mensagens de erro ao usuário', () => {
     cy.loginErro('kiz@testmail.com', 'Test123');
   })
+
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', ()=> {
+    cy.login('emailincorreto@testmail.com', 'Test123');
+    cy.wait('@stubPost');
+    cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
+  })
+
 })
